643a587ef9a81905288cf7e962893be7bfecba97
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2025-04-21 11:39:28 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by noureldeen abouelkassem on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
